0029cba9ea6fe2d275bd11610478666ee5d6f183
julianarce1995 <100883327+julianarce1995@users.noreply.github.com>
2022-06-17 12:11:20 -0500

    add productos

--- src/js/controladorIndex.js
+++ src/js/controladorIndex.js
@@ -1,9 +1,241 @@
-//ENTRADAS == VARIABLES
-var nombre="JAMES RODRIGUEZ"
-
-if(true){
-    var nombre="Lucho diaz"
-}
-console.log(`hola ${nombre}`)
-console.log("hola")
-console.log("adios")
+let producto = [{
+    Nombre: 'Funko Pop Petro',
+    Valor: 45000,
+    Cantidad: 5,
+    Imagen: 'https://pbs.twimg.com/profile_images/1519369348422459394/lJXX7Q7z_400x400.jpg',
+    Descripcion: 'Un Funko de petro'
+},{
+    Nombre: 'Funko Pop Duque',
+    Valor: 5000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Duque'
+},{
+    Nombre: 'Funko Pop Goku',
+    Valor: 40000,
+    Cantidad: 12,
+    Imagen: 'https://falabella.scene7.com/is/image/FalabellaCO/12099939_1?wid=800&hei=800&qlt=70',
+    Descripcion: 'Un Funko de Goku'
+},{
+    Nombre: 'Funko Pop Gohan',
+    Valor: 35000,
+    Cantidad: 17,
+    Imagen: '',
+    Descripcion: 'Un Funko de gohan'
+},{
+    Nombre: 'Funko Pop Link',
+    Valor: 55000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Link'
+},{
+    Nombre: 'Funko Pop Zelda',
+    Valor: 75000,
+    Cantidad: 14,
+    Imagen: '',
+    Descripcion: 'Un Funko de Zelda'
+},{
+    Nombre: 'Funko Pop Banjoo',
+    Valor: 15000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Banjoo'
+},{
+    Nombre: 'Funko Pop Picolo',
+    Valor: 15000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Picolo'
+},{
+    Nombre: 'Funko Pop Vegeta',
+    Valor: 45000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Vegeta'
+},{
+    Nombre: 'Funko Pop Bulma',
+    Valor: 15000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Bulma'
+},{
+    Nombre: 'Funko Pop Milk',
+    Valor: 45000,
+    Cantidad: 5,
+    Imagen: '',
+    Descripcion: 'Un Funko de Milk'
+},{
+    Nombre: 'Funko Pop Trunks',
+    Valor: 45000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Trunks'
+},{
+    Nombre: 'Funko Pop Videl',
+    Valor: 40000,
+    Cantidad: 12,
+    Imagen: '',
+    Descripcion: 'Un Funko de Videl'
+},{
+    Nombre: 'Funko Pop Pan',
+    Valor: 35000,
+    Cantidad: 17,
+    Imagen: '',
+    Descripcion: 'Un Funko de Pan'
+},{
+    Nombre: 'Funko Pop Goten',
+    Valor: 55000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Goten'
+},{
+    Nombre: 'Funko Pop ',
+    Valor: 75000,
+    Cantidad: 14,
+    Imagen: '',
+    Descripcion: 'Un Funko de Zelda'
+},{
+    Nombre: 'Funko Pop Banjoo',
+    Valor: 15000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Banjoo'
+},{
+    Nombre: 'Funko Pop Picolo',
+    Valor: 15000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Picolo'
+},{
+    Nombre: 'Funko Pop ',
+    Valor: 5000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Duque'
+},{
+    Nombre: 'Funko Pop Duque',
+    Valor: 5000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Duque'
+},{
+    Nombre: 'Funko Pop Petro',
+    Valor: 45000,
+    Cantidad: 5,
+    Imagen: 'https://pbs.twimg.com/profile_images/1519369348422459394/lJXX7Q7z_400x400.jpg',
+    Descripcion: 'Un Funko de petro'
+},{
+    Nombre: 'Funko Pop Duque',
+    Valor: 5000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Duque'
+},{
+    Nombre: 'Funko Pop Goku',
+    Valor: 40000,
+    Cantidad: 12,
+    Imagen: 'https://falabella.scene7.com/is/image/FalabellaCO/12099939_1?wid=800&hei=800&qlt=70',
+    Descripcion: 'Un Funko de Goku'
+},{
+    Nombre: 'Funko Pop Gohan',
+    Valor: 35000,
+    Cantidad: 17,
+    Imagen: '',
+    Descripcion: 'Un Funko de gohan'
+},{
+    Nombre: 'Funko Pop Link',
+    Valor: 55000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Link'
+},{
+    Nombre: 'Funko Pop Zelda',
+    Valor: 75000,
+    Cantidad: 14,
+    Imagen: '',
+    Descripcion: 'Un Funko de Zelda'
+},{
+    Nombre: 'Funko Pop Banjoo',
+    Valor: 15000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Banjoo'
+},{
+    Nombre: 'Funko Pop Picolo',
+    Valor: 15000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Picolo'
+},{
+    Nombre: 'Funko Pop ',
+    Valor: 5000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Duque'
+},{
+    Nombre: 'Funko Pop Duque',
+    Valor: 5000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Duque'
+},{
+    Nombre: 'Funko Pop Petro',
+    Valor: 45000,
+    Cantidad: 5,
+    Imagen: 'https://pbs.twimg.com/profile_images/1519369348422459394/lJXX7Q7z_400x400.jpg',
+    Descripcion: 'Un Funko de petro'
+},{
+    Nombre: 'Funko Pop Duque',
+    Valor: 5000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Duque'
+},{
+    Nombre: 'Funko Pop Goku',
+    Valor: 40000,
+    Cantidad: 12,
+    Imagen: 'https://falabella.scene7.com/is/image/FalabellaCO/12099939_1?wid=800&hei=800&qlt=70',
+    Descripcion: 'Un Funko de Goku'
+},{
+    Nombre: 'Funko Pop Gohan',
+    Valor: 35000,
+    Cantidad: 17,
+    Imagen: '',
+    Descripcion: 'Un Funko de gohan'
+},{
+    Nombre: 'Funko Pop Link',
+    Valor: 55000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Link'
+},{
+    Nombre: 'Funko Pop Zelda',
+    Valor: 75000,
+    Cantidad: 14,
+    Imagen: '',
+    Descripcion: 'Un Funko de Zelda'
+},{
+    Nombre: 'Funko Pop Banjoo',
+    Valor: 15000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Banjoo'
+},{
+    Nombre: 'Funko Pop Picolo',
+    Valor: 15000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Picolo'
+},{
+    Nombre: 'Funko Pop ',
+    Valor: 5000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Duque'
+},{
+    Nombre: 'Funko Pop Duque',
+    Valor: 5000,
+    Cantidad: 1,
+    Imagen: '',
+    Descripcion: 'Un Funko de Duque'
+}]
